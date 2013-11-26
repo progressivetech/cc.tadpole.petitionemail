133e845de011e6d0f48e88c9b4d5ceca31ed44a8
Joseph Lacey <joseph@palantetech.coop>
2013-11-26 12:51:58 -0500

    Adding Uninstall procedure.  Alpha release.

--- CRM/Petitionemail/Upgrader.php
+++ CRM/Petitionemail/Upgrader.php
@@ -19,9 +19,9 @@ class CRM_Petitionemail_Upgrader extends CRM_Petitionemail_Upgrader_Base {
 
   /**
    * Example: Run an external SQL script when the module is uninstalled
-   *
+   */
   public function uninstall() {
-   $this->executeSqlFile('sql/myuninstall.sql');
+    $this->executeSqlFile('sql/Petitionemail_uninstall.sql');
   }
 
   /**
